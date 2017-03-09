3f0f0a390add7b31d0e627594bfcade345b648c5
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-03-09 13:49:59 +0100

    add gpl

--- src/main/java/de/citec/csra/rst/parse/PayloadParser.java
+++ src/main/java/de/citec/csra/rst/parse/PayloadParser.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2017 pholthau
+/* 
+ * Copyright (C) 2017 Patrick Holthaus
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
