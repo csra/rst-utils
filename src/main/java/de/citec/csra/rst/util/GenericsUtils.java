5809f7bde6651f89f21b97daa41342678ba5afa1
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-03-14 11:28:22 +0100

    package move

--- src/main/java/de/citec/csra/rst/util/GenericsUtils.java
+++ src/main/java/de/citec/csra/rst/util/GenericsUtils.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.citec.csra.rst.parse;
+package de.citec.csra.rst.util;
 
 import com.google.protobuf.ByteString;
 import rst.generic.KeyValuePairType;
