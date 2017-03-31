2c6c5fa6241891075db7b216a835e0b434bf22ca
Patrick Holthaus <patrick.holthaus@uni-bielefeld.de>
2017-03-31 09:34:23 +0200

    introduce more general parse method * loads converters automatically  * tries to parse numbers and booleans

--- src/main/java/de/citec/csra/rst/util/TypesUtils.java
+++ src/main/java/de/citec/csra/rst/util/TypesUtils.java
@@ -19,8 +19,14 @@ package de.citec.csra.rst.util;
 import com.google.protobuf.Message;
 import com.google.protobuf.TextFormat;
 import java.lang.reflect.InvocationTargetException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import rsb.converter.DefaultConverterRepository;
+import rsb.converter.ProtocolBufferConverter;
 
 /**
  *
@@ -29,30 +35,84 @@ import java.util.logging.Logger;
 public class TypesUtils {
 
 	private final static Logger LOG = Logger.getLogger(TypesUtils.class.getName());
-
+	private final static Pattern RST = Pattern.compile("^\\.(.*):\\{(.*)\\}$");
+	
+	@Deprecated
 	public static Object parseText(String definition) throws IllegalArgumentException {
 		if (definition.startsWith(".")) {
 			try {
 				String rstName = definition.substring(definition.indexOf(".") + 1, definition.indexOf(":"));
 				String typeInfo = definition.substring(definition.indexOf("{") + 1, definition.lastIndexOf("}"));
-				LOG.log(Level.FINER, "Assuming RST of type ''{0}'' specified as ''{1}''.", new Object[]{rstName, typeInfo});
-				
+				LOG.log(Level.INFO, "Assuming RST of type ''{0}'' specified as ''{1}''.", new Object[]{rstName, typeInfo});
+
 				String pkg = rstName.substring(0, rstName.lastIndexOf(".") + 1);
 				String clz = rstName.substring(rstName.lastIndexOf(".") + 1);
 				String fqClz = pkg + clz + "Type$" + clz;
-				
-				LOG.log(Level.FINER, "Trying to instantiate builder for class ''{0}''.", fqClz);
-				
+
+				LOG.log(Level.INFO, "Trying to instantiate builder for class ''{0}''.", fqClz);
+
 				Class<?> cls = Class.forName(fqClz);
 				Message.Builder msgBuilder = (Message.Builder) cls.getMethod("newBuilder").invoke(null);
 				TextFormat.merge(typeInfo, msgBuilder);
 				return msgBuilder.build();
 			} catch (StringIndexOutOfBoundsException | ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | InvocationTargetException | TextFormat.ParseException e) {
-				LOG.log(Level.FINER, "Unable to parse string ''{0}'' as an rst data type ({1}), returning original definiton.", new Object[]{definition, e.getMessage()});
+				LOG.log(Level.INFO, "Unable to parse string ''{0}'' as an rst data type ({1}), returning original definiton.", new Object[]{definition, e});
 				return definition;
 			}
 		} else {
 			return definition;
+		}
+	}
+
+	public static Object parse(String dsc) {
+		if (dsc == null) {
+			LOG.log(Level.INFO, "Description is null, returning.");
+			return dsc;
+		}
+
+		try {
+			return NumberFormat.getInstance().parse(dsc);
+		} catch (ParseException ex) {
+			LOG.log(Level.INFO, "Could not infer number from description string ''{0}''.", dsc);
+		}
+
+		switch (dsc.toLowerCase()) {
+			case "false":
+				return false;
+			case "true":
+				return true;
+			default:
+				LOG.log(Level.INFO, "Could not infer boolean from description string ''{0}''.", dsc);
+		}
+
+        Matcher matcher = RST.matcher(dsc);
+		if (matcher.matches()) {
+			try {
+				String rstName =  matcher.group(1);
+				String typeInfo = matcher.group(2);
+				LOG.log(Level.INFO, "Assuming RST of type ''{0}'' specified as ''{1}''.", new Object[]{rstName, typeInfo});
+
+				String pkg = rstName.substring(0, rstName.lastIndexOf(".") + 1);
+				String clz = rstName.substring(rstName.lastIndexOf(".") + 1);
+				String fqClz = pkg + clz + "Type$" + clz;
+
+				LOG.log(Level.INFO, "Trying to instantiate builder for class ''{0}''.", fqClz);
+
+				Class<?> cls = Class.forName(fqClz);
+				Message.Builder msgBuilder = (Message.Builder) cls.getMethod("newBuilder").invoke(null);
+				TextFormat.merge(typeInfo, msgBuilder);
+				
+				Message msg = msgBuilder.build();
+				DefaultConverterRepository.getDefaultConverterRepository().addConverter(new ProtocolBufferConverter<>(msg));
+				LOG.log(Level.INFO, "Loaded default converter for message ''{0}''.", StringRepresentation.shortString(msg));
+				return msg;
+			} catch (StringIndexOutOfBoundsException | ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | InvocationTargetException | TextFormat.ParseException e) {
+				LOG.log(Level.INFO, "Unable to parse string ''{0}'' as an rst data type ({1}), returning original definiton.", new Object[]{dsc, e});
+				return dsc;
+			}
+		} else {
+			LOG.log(Level.INFO, "Could not infer rst from description string ''{0}''.", dsc);
+			return dsc;
 		}
 	}
 }
